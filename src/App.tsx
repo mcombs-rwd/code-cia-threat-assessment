864e0c6ef31dfc760abe4a75bdf39e5d3fd752cc
Mike Combs <mcombs964@gmail.com>
2023-11-21 01:45:56 -0500

    enable shadows

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <div className="canvas-container">
         <Suspense fallback={<Loading />}>
-          <Canvas>
+          <Canvas shadows>
             <Html>
               <h1>Test</h1>
             </Html>
