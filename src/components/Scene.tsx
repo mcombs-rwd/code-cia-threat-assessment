c56aa0a0a0620e2fb3b158b8bd14b33c149f8bd4
Mike Combs <mcombs964@gmail.com>
2023-11-21 00:08:53 -0500

    wip: box react to hover and click

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -6,16 +6,16 @@ import Box from "../models/Box";
 function Scene() {
   return (
     <>
-      <ambientLight color={"white"} intensity={0.2} />
+      <ambientLight color={"white"} intensity={0.3} />
       <spotLight
         color={"white"}
-        intensity={1}
-        distance={7.0}
-        position={[-1.2, 1, 0]}
+        intensity={5}
+        distance={6.0}
+        position={[-1, 0.5, 1.0]}
         penumbra={0.0}
         castShadow
       />
-      <Box position={[-2.5, 0, 0]} />
+      <Box position={[3, 0, 2]} />
       <Environment background>
         <mesh>
           <sphereGeometry args={[50, 32, 32]} />
--- src/models/Box.tsx
+++ src/models/Box.tsx
@@ -1,7 +1,6 @@
-// import { useRef, useState } from "react";
+import { useState } from "react";
 import type { ComponentPropsWithoutRef } from "react";
 interface MeshProps extends ComponentPropsWithoutRef<"mesh"> {
-  // props: string;
 }
 // import { useFrame } from "@react-three/fiber";
 
@@ -9,8 +8,8 @@ function Box({ ...props }: MeshProps) {
   // This reference gives us direct access to the THREE.Mesh object
   // const meshRef = useRef();
   // Hold state for hovered and clicked events
-  // const [isHovered, setHovered] = useState(false);
-  // const [isActive, setActive] = useState(false);
+  const [isHovered, setHovered] = useState(false);
+  const [isActive, setActive] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
   // useFrame((state, delta) => (meshRef.current.rotation.x += delta));
   // Return the view, these are regular Threejs elements expressed in JSX
@@ -19,17 +18,15 @@ function Box({ ...props }: MeshProps) {
       {...props}
       castShadow
       receiveShadow
-      scale={1}
+      scale={isActive ? 1.5 : 1}
+      onClick={() => setActive(!isActive)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
     >
       <boxGeometry args={[1, 1.5, 2.5]} /> {/* width, height, depth */}
-      <meshStandardMaterial color={"hotpink"} />
+      <meshStandardMaterial color={isHovered ? "hotpink" : "orange"} />
     </mesh>
   );
 }
 
 export default Box;
-
-// ref={meshRef}
-// onClick={(event) => setActive(!isActive)}
-// onPointerOver={(event) => setHovered(true)}
-// onPointerOut={(event) => setHovered(false)}
